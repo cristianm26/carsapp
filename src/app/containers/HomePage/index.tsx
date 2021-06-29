f7093d67e7af17458f1a9b35156ffb9470d0d28c
hugo@DESKTOP-J7IENLF <cristian260893@gmail.com>
2021-06-29 12:26:14 -0500

    topsection

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import styled from 'styled-components';
 import tw from 'twin.macro';
+import BookCard from '../../components/bookCard';
+import Marginer from '../../components/marginer';
 import { NavBar } from '../../components/NavBar';
+import  { TopSection } from './topSections';
 
 const PageContainer = styled.div`
     ${tw`
@@ -14,8 +17,14 @@ const PageContainer = styled.div`
     `}
 `;
 
-export function HomePage(){
-    return <PageContainer>
-        <NavBar/>
+export const HomePage =()=>{
+    return (
+<PageContainer>
+        <NavBar />
+       <TopSection/>
+       <Marginer direction="vertical" margin="4em" />
+      <BookCard />
+      
     </PageContainer>
+    )
 }
